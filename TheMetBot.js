9624490fa51d632a11775e97b0cc822c9e93c380
unknown <sam.caddeo@edu.ece.fr>
2019-02-11 14:48:06 +0100

    ajout code post image

--- TheMetBot.js
+++ TheMetBot.js
@@ -13,11 +13,43 @@ const Twitter = new twit(config);
 //  get access to the MET API
 //
 
-  var img ='https://source.unsplash.com/random';
-  Twitter.post('statuses/update', { status: img }, function(err, data, response) {
-    console.log(img)
+
+//
+// post a tweet with media
+//
+var b64content = fs.readFileSync('https://source.unsplash.com/random', { encoding: 'base64' })
+
+// first we must post the media to Twitter
+T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+  // now we can assign alt text to the media, for use by screen readers and
+  // other text-based presentations and interpreters
+  var mediaIdStr = data.media_id_string
+  var altText = "PicBot"
+  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+  T.post('media/metadata/create', meta_params, function (err, data, response) {
+    if (!err) {
+      // now we can reference the media and post a tweet (media will attach to the tweet)
+      var params = { status: 'test', media_ids: [mediaIdStr] }
+
+      T.post('statuses/update', params, function (err, data, response) {
+        console.log(data)
+      })
+    }
+  })
 })
 
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+var url = 'https://source.unsplash.com/random';
+var pic = console.createElement('img');
+pic.src = url;
+
+  Twitter.post('statuses/update', { status: pic }, function(err, data, response) {
+    console.log(pic)
+})
+*/
 
 /*
 const fetch = require('node-fetch')
