2deda978b0e01b641ccd45145386c3023b9a683d
DESKTOP-M2JB4NO\SamK <samkdo771@gmail.com>
2019-02-11 13:12:10 +0100

    choix api data

--- TheMetBot.js
+++ TheMetBot.js
@@ -1,10 +1,10 @@
 const twit = require('twit');
 
 const config = {
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret
+  consumer_key: '9a34Su8Gu3FWZAZh9zIfZTGxY', //process.env.consumer_key,
+  consumer_secret: 'KnPEFV4x4xkmgiQFdpbrOFTUc6OAxlCc8jHyh674JHGEmDJcaK',//process.env.consumer_secret,
+  access_token: '1093781965164158976-1CwsW3No82AqxaU5s0krTQsgEMjViC',//process.env.access_token,
+  access_token_secret: 'Hjk5XAfQGMLODy75h9We8UHZqwfZGOfbfH0R16sO0gs5G' //process.env.access_token_secret
 }
 
 const Twitter = new twit(config);
@@ -13,18 +13,24 @@ const Twitter = new twit(config);
 //  get access to the MET API
 //
 
+  var img ='https://source.unsplash.com/random';
+  Twitter.post('statuses/update', { status: img }, function(err, data, response) {
+    console.log(img)
+})
+
+
+/*
 const fetch = require('node-fetch')
-//for(let i=1; i<5; i++){
-fetch('http://poetrydb.org/title') // Call the fetch function passing the url of the API as a parameter
+fetch('https://source.unsplash.com/random') // Call the fetch function passing the url of the API as a parameter
 .then(res => res.json())//response type
 .then(data => 
-    //Twitter.post('statuses/update', { status: data }, function(err, data, response) {
+    Twitter.post('statuses/update', { status: data }, function(err, data, response) {
       console.log(data)
- //})
+ })
  )
  .catch(err => console.log(err)
  );
-
+*/
 //
 //  tweet 'hello world!'
 //
